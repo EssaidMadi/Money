2c70e416b145aadd68e024e7e0abf5e64aeee3eb
essaidmadi <essaid.madi@yahoo.com>
2023-03-08 15:20:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+
+function App() {
+  const Layout = () => {
+    return (
+      <>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
